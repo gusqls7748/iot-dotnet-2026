fbc741ec653e1644c2da54d64817ba7e7295531e
chen hyenbeen <123910896+gusqls7748@users.noreply.github.com>
2026-06-02 17:24:46 +0900

    6일차 커밋

---
+++ webapp/IotWpfSolutions/WpfBasic02Navi/SubPage01.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBasic02Navi
+{
+    /// <summary>
+    /// SubPage01.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SubPage01 : Page
+    {
+        public SubPage01()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer.Source = new Uri(@".\sample_360p.mp4", UriKind.RelativeOrAbsolute);
+            MediaPlayer.Play();
+        }
+    }
+}
