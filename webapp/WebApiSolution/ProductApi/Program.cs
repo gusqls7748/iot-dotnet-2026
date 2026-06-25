aa13d649220bb04f287a6310560e40d29cffa61c
chen hyenbeen <123910896+gusqls7748@users.noreply.github.com>
2026-06-25 17:23:39 +0900

    커밋

---
+++ webapp/WebApiSolution/ProductApi/Program.cs
@@ -0,0 +1,39 @@
+
+namespace ProductApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var connString = builder.Configuration.GetConnectionString("TestDbConnection");
+
+            Console.WriteLine(connString);
+
+            // Add services to the container.
+            // Bulider.Services.AddDbContext<> Enity
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
